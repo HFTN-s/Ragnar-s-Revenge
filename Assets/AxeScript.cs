b04b920238bae8894dd7badd5de7e180decd144c
next-gen-gaming-admin <redwar986@gmail.com>
2024-03-22 19:34:05 +1100

    Version 0.11
    Player Movement, Rotation added
    Axe no longer freeze after impact forever.
    Axe needs force in order to stick into certain objects (walls not yet).
    Shortened player reach

---
+++ Assets/AxeScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class AxeScript : MonoBehaviour
+{
+    [SerializeField] private GameObject bladeObject;
+    private Rigidbody rb;
+    private Collider bladeCollider;
+    [SerializeField] private int minimumForce = 1;
+    [SerializeField] private XRGrabInteractable axeGrabInteractable;
+    private AudioSource audioSource; // Audio source to play sounds
+    [SerializeField] private AudioClip[] axeSounds;
+
+    void Start()
+    {
+        bladeCollider = bladeObject.GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
+        axeGrabInteractable.onSelectEntered.AddListener(HandleAxeGrabbed);
+        audioSource = gameObject.AddComponent<AudioSource>(); // Initialize the AudioSource
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (bladeCollider.bounds.Intersects(other.collider.bounds))
+        {
+            Debug.Log("Blade hit something!");
+            foreach (ContactPoint contact in other.contacts)
+            {
+                if (other.gameObject.tag == "Wood" && rb.velocity.magnitude > minimumForce)
+                {
+                    Debug.Log("Hit Wood");
+                    FreezeRigidbody();
+                    PlayAxeSound(); // Play the sound effect
+                    break;
+                }
+            }
+        }
+    }
+
+    private void FreezeRigidbody()
+    {
+        rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+    }
+
+    private void PlayAxeSound()
+    {
+        if (audioSource != null && axeSounds.Length > 0)
+        {
+            // Play a random sound from the axeSounds array
+            audioSource.clip = axeSounds[Random.Range(0, axeSounds.Length)];
+            audioSource.Play();
+        }
+    }
+
+    void HandleAxeGrabbed(XRBaseInteractor interactor)
+    {
+        Debug.Log("Axe Grabbed");
+        rb.constraints = RigidbodyConstraints.None;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private InputDevice rightHandController;
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float rotationAmount = 15.0f;
+    private Vector2 turnInput;
+
+    void Start()
+    {
+        SetupRightHandController();
+    }
+
+    void SetupRightHandController()
+    {
+        rightHandController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        if (!rightHandController.isValid)
+        {
+            Debug.Log("Right hand controller not found. Retrying...");
+            Invoke("SetupRightHandController", 0.5f); // Retry after a delay
+        }
+        else
+        {
+            Debug.Log("Right hand controller found");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!rightHandController.isValid) return; // Exit if controller is not valid
+
+        if (rightHandController.TryGetFeatureValue(CommonUsages.primaryButton, out bool isAPressed) && isAPressed)
+        {
+            Debug.Log("Moving Forward");
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (rightHandController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 joystickValue))
+        {
+            RotatePlayer(joystickValue);
+        }
+    }
+
+    void RotatePlayer(Vector2 joystickValue)
+    {
+        if (joystickValue.x > 0.5f)
+        {
+            transform.Rotate(Vector3.up, rotationAmount);
+        }
+        else if (joystickValue.x < -0.5f)
+        {
+            transform.Rotate(Vector3.up, -rotationAmount);
+        }
+    }
+}
