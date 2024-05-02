c08e329e5b5b974aeba2dd65fb760d6afe2a44ca
next-gen-gaming-admin <redwar986@gmail.com>
2024-05-02 12:36:17 +1000

    Removed Some Assets

---
+++ Assets/Scripts/Rb_AddForce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rb_AddForce : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //add force in forward direction
+        rb.AddForce(transform.forward * -1000);
+        
+    }
+}
